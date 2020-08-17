822155701763cb92419577878bd5cc3dde32c5ef
Connor <59297927+Gabertho@users.noreply.github.com>
2020-08-17 12:22:26 -0300

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,25 +1,25 @@
 import tweepy
 import time 
 
-auth = tweepy.OAuthHandler('uOx4nLBThjG45o17AleIe0FDr','326F8cSpfD1zrpB41JKFuXHDFdC9ea223mYiQdcO94mFI8nEez')
+auth = tweepy.OAuthHandler('YOUR PERSONAL API KEY','YOUR PERSONAL API KEY PASSWORD')
 
-auth.set_access_token('1295218133830180865-ZPfzVdX5WPFRkD1zZqMbf5K9WtQmYT','EnEZaW5MRGRSDrJVAsPo1LzZVqCAMgRkgkOiB6jNNOxEA')
+auth.set_access_token('YOUR PERSONAL ACCESS TOKEN','YOUR PERSONAL ACCES TOKEN PASSWORD')
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True) #Guarantee that we will not reach the maximum allowed requests from Twitter API.
+
 
 user = api.me
 
-search = 'Brasil'
-nrTweets= 1000
-
+search = 'Brasil' #The word we are going to search on tweets.
+nrTweets= 1000 #The maximum number of tweets that we are going to search.
 for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
     try:
         print('Tweet Liked')
-        tweet.favorite()
+        tweet.favorite() 
         print('Tweet retweeted')
         tweet.retweet()
-        time.sleep(10)
+        time.sleep(10) #Pause os 10 seconds.
     except tweepy.TweepError as e:
-        print(e.reason)
+        print(e.reason) #Prints the error reason.
     except StopIteration:
-        break
+        break #Exit the Iteration
